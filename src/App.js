d89cc4686e81ba893fef352f4be269757eb720d5
Haroon Ahmed <haroonrmit@gmail.com>
2018-10-08 04:22:57 +0400

    used map to create multiple player componets

--- src/App.js
+++ src/App.js
@@ -3,12 +3,41 @@ import './App.css';
 import Header from './components/Header';
 import Player from './components/Player';
 
+const players = [
+  {
+    name: "Haroon",
+    score: 50,
+    id: 1
+  },
+  {
+    name: "Haza",
+    score: 85,
+    id: 2
+  },
+  {
+    name: "Ahmed",
+    score: 95,
+    id: 3
+  },
+  {
+    name: "Zaman",
+    score: 80,
+    id: 4
+  }
+]
+
 class App extends Component {
   render() {
     return (
       <div>
         <Header title="Scoreboard" player={1}/>
-        <Player playerName={"Haroon"} score={25}/>
+        {players.map((player) => {
+          return <Player 
+          playerName={player.name} 
+          score={player.score}
+          key={player.id.toString()}
+          />
+        })}
       </div>
     );
   }
